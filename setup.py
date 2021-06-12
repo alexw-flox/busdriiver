32672e1ca1a79db46e1772ed78afd069167319ff
alexw-flox <alexw@floxdev.com>
2021-06-12 15:18:43 +0000

    initial setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="busdriver",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'busdriver = busdriver:main',
+        ],
+    }
+)
+

2f5576d52ecc625c8a4bd7ea014baad75d4421cc
alexw-flox <alexw@floxdev.com>
2021-06-12 16:36:27 +0000

    Deck is implemented

--- busdriver/driver.py
+++ busdriver/driver.py
@@ -1,4 +1,49 @@
+from enum import Enum
+from random import shuffle
 
+
+class Suit(Enum):
+    CLUB = 0
+    DIAMOND = 1
+    HEART = 2
+    SPADE = 3
+    
+    def __gt__(self, other):
+        return self.value > other.value
+
+class Card:
+    def __init__(self, suit, rank):
+        if not isinstance(rank, int):
+            raise TypeError
+        self.suit = suit
+        self.rank = rank
+    def __repr__(self):
+        return '({}, {})'.format(str(self.suit.name), str(self.rank))
+    def __gt__(self, other):
+        return (self.suit, self.rank) > (other.suit, other.rank)
+
+class Deck:
+    def __init__(self):
+        self.total = 52
+
+        cards = list(range(52))
+        shuffle(cards)
+        self.remnant = cards
+        self.next_idx = 0
+
+    def idx_to_card(self, idx):
+        return Card(Suit(idx // 13), idx % 13)
+
+    def draw_card(self):
+        selected = self.idx_to_card(self.remnant[self.next_idx])
+        self.next_idx += 1
+        return selected
 
 def main():
-    print('hello')
+    d = Deck()
+    for i in range(52):
+        card = d.draw_card()
+        print(card)
+
+if __name__=='__main__':
+    main()
